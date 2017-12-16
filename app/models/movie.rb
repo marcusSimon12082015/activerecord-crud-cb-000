16b4c348b9dd94cd7776ea5929583022d3a509ba
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-16 13:22:42 +0000

    automatically backed up by learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+#  ATTRIBUTES = ["title", "release_date", "director", "lead", "in_theaters"]
+
+#  ATTRIBUTES.each do |col_name|
+#    attr_accessor col_name.to_sym
+#  end
+
+#  def initialize(attributes={})
+#    attributes.each do |property,value|
+#      self.send("#{property}=",value)
+#    end
+#  end
 end
